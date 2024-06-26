6288e9d34f242f5a8b83a8da4e2cca467b3e1b92
arulprofessional <arulprofessional@gmail.com>
2024-06-26 10:32:09 +0530

    Updated with spinner gif image

--- src/LoadingSpinner.jsx
+++ src/LoadingSpinner.jsx
@@ -4,16 +4,18 @@ export default function LoadingSpinner() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(
-      'https://react-getting-started-ccd14-default-rtdb.firebaseio.com/contacts.json'
-    ).then((response) => {
-      setIsLoading(false);
-    });
+      setTimeout(() => {
+        setIsLoading(false);
+        setContactList(contacts);
+      }, 3000);
   }, []);
 
-  let content = <div>Loading . . . </div>;
+  let content = <img
+  src="https://miro.medium.com/v2/resize:fit:1400/1*CsJ05WEGfunYMLGfsT2sXA.gif"
+  alt="Loading-Spinner"
+/>;
   if (!isLoading) {
-    content = <div>Loaded</div>;
+    content = <h1>Loaded</h1>;
   }
 
   return (
